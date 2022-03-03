884754bb4c5fc36c58793cf2fb062644bea8c032
Sherry <sherry9624715@gmail.com>
2022-03-03 18:20:00 +0800

    Fetch data and process from endpoint

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
-const endpoint = "https://script.googleusercontent.com/macros/echo?user_content_key=pPE-mribkVd6h-aAff49Ox3AL4iLtykzeDE7gHn8aIc23rLh0yTGOH_BvKna3W9FAoFF1QEmvR2LdqH6AWkQP3EPfNK4AOrIm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnI1YuMnJASWge9fMETO9NUOAGJBRRI1bRajmL4hlTdkR7fhkl9grxXV7PlHBbeW7vOJrFkQWlLvh2ZlyfCl_y-aZYIgnXMHJFdz9Jw9Md8uu&lib=MrkjvEoks_SWqD99E0xcSU7MqtozM3Ode"
+//GAS: https://script.google.com/home/projects/1FpqqjMCtU8xqs2lS7G-7mPZk61pcJKb2bWyHBsWZMBIYci0fdv0V4dND/edit
+
+const endpoint = "https://script.google.com/macros/s/AKfycbyCE8THCb3af17Q0qLOeayKFSlxVQg2yQNZ6sd1jjy51N8I98cdaKr0FXySNt92qfQG/exec"
 const output = document.querySelector('.output');
 const btn = document.querySelector('button');
 console.log(btn);
 
-btn.addEventListener('click',(e) =>{
+btn.addEventListener('click',function(){fetchEndpointData()})
+
+function fetchEndpointData(){
     console.log("clicked!!")
     fetch(endpoint)
         .then(res => res.json())
         .then(data => {
             console.log(data);
-            var recommand_dosage = response.results[0].properties.dosage-mgkg.rich_text[0].plain_text;
-            var 
-
+            printNeededData(data);
         })
-})
+}
+
+function printNeededData(data){
+    var dosage = data.results[0].properties.dosage_mgkg.rich_text[0].plain_text;
+    console.log(`dosage:${dosage}`);
+    
+    var prefer_dosage = data.results[0].properties.prefer_dosage.rich_text[0].plain_text;
+    console.log(`prefer dosage:${prefer_dosage}`);
+    
+    var injection_way = [];
+    var injection_way_raw_array = data.results[0].properties.injection_way.multi_select;
+    injection_way_raw_array.forEach(element => injection_way.push(element.name));
+    console.log(injection_way);
+    
+    var medicine_name = data.results[0].properties.medicine_name.title[0].plain_text;
+    console.log(`medicine name:${medicine_name}`);
+    
+    var density = data.results[0].properties.density_mgml.number;
+    console.log(`density:${density}`);
+    //TODO: need solution for null value
+}
+
