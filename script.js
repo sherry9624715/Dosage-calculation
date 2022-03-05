8b7c6fd6f72e303608f29148d71a88a4ff82609e
Sherry <sherry9624715@gmail.com>
2022-03-05 19:05:07 +0800

    Complete calculate system and basic appearence.

--- script.js
+++ script.js
@@ -1,72 +1,108 @@
 //GAS: https://script.google.com/home/projects/1FpqqjMCtU8xqs2lS7G-7mPZk61pcJKb2bWyHBsWZMBIYci0fdv0V4dND/edit
 
-const endpoint = "https://script.google.com/macros/s/AKfycbyCE8THCb3af17Q0qLOeayKFSlxVQg2yQNZ6sd1jjy51N8I98cdaKr0FXySNt92qfQG/exec"
-const post_endpoint = "https://script.google.com/macros/s/AKfycbwca8wUcnMhksKgfx3hw6b-tLnfdRqrAJ5hsnn2xX6x6-9uBD1hXCx6tLb_d7-aOuG7tg/exec"
-const drug_list = ["drug","Midazolam","yao"];
+const endpoint = "https://script.google.com/macros/s/AKfycbxnrhl2fZn4Udk9Xqg8rHOFZrrAuHGaqRbzL8vPjzHe8PUpRRZq2bjyXXmcmYiRuoHxaQ/exec"
 const output = document.querySelector('.output');
 const btn_holder = document.querySelector('.drug-button-holder');
 const c_result = document.querySelector('.calculation-result');
 const input = document.querySelector('.weight');
+const adjust_field = document.querySelector('.adjust-dosage');
+const drug_list = ['Ketamine >', 'Alphaxalone <', 'Propofol >', 'Alphaxalone >', 'Ketamine <', 'Propofol <', 'Butorphanol <', 'Butorphanol >20kg', 'Fentanyl / L', 'Fentanyl / S', 'Hydromorphone 大', 'Hydromorphone 小', 'Methadone >', 'Methadone <', 'Midazolam', 'Dexmedetomidine > 20', 'Dexmedetomidine < 20', 'Acepromazine  > 20 kg', 'Acepromazine  < 20 kg'];
+//run getMedList() in console to update
 
+//add body surface area after UI is okay
+//add 常用組合（ex for 胰臟炎）
 
-drug_list.forEach(drug => {
-    var btn = document.createElement('button');
-    const newContent = document.createTextNode(drug);
-    btn.setAttribute("id", drug);
-    btn.appendChild(newContent);
-    btn_holder.appendChild(btn);
-});
+printButtons();
 
-const btns = document.querySelectorAll('button');
-// btn.addEventListener('click',function(){fetchEndpointData()})
-console.log(btns);
-btns.forEach(btn => btn.addEventListener('click',function(){postDataToEndpoint(btn.id)}));
-
-function calculateDosage(data){
-    var dosage = data.results[0].properties.dosage_mgkg.rich_text[0].plain_text;
-    var prefer_dosage = data.results[0].properties.prefer_dosage.rich_text[0].plain_text;
-    var density = data.results[0].properties.density_mgml.number;
-    var weight = input.value;
-    var dosage = weight*parseFloat(prefer_dosage)/parseFloat(density);
-    c_result.innerHTML = `<div>weight:${dosage} ml</div>`
+function printButtons(){
+    drug_list.forEach(drug => addDrugButton(drug));
+    const btns = document.querySelectorAll('button');
+    // btn.addEventListener('click',function(){fetchEndpointData()})
+    console.log(btns);
+    btns.forEach(btn => btn.addEventListener('click',function(){
+        postDataToEndpoint(btn.id);
+    }));   
 }
+
+
+function getMedList(){
+    fetch(endpoint)
+        .then(res => res.json())
+        .then (data => {
+            console.log(data.med_list);
+        })
+}   
+
 
 function postDataToEndpoint(drug_name){
     showLoading();
     const myobj = {name:drug_name};
-    fetch(post_endpoint, {
+    fetch(endpoint, {
         method: 'POST',
         body: myobj.name
     })
         .then(res => res.json())
         .then (data => {
-        console.log(data);
-        console.log(JSON.stringify(data));
-        printNeededData(data);
-        showDrugData(data);
-        calculateDosage(data);
+            data.weight = input.value;
+            console.log(data);
+            console.log(JSON.stringify(data));
+            //add null error
+            try{
+                var adjust = document.querySelector(".adjust-field");
+                adjust.value = "";
+                adjust.placeholder = data.prefer_dosage;
+            }
+            finally{
+                showDrugData(data);
+                calculateDosage(data, data.prefer_dosage);
+                addDosageAdjustField(data);
+                input.addEventListener("input",function(){calculateDosage(data)})
+            }
         })
-        
+    }   
+
+function addDosageAdjustField(data){
+    var newDosageInput = document.createElement('input');
+    newDosageInput.setAttribute("type", 'number');
+    newDosageInput.setAttribute("placeholder", 'Type new dosage here');
+    // newDosageInput.setAttribute("value", data.prefer_dosage);
+    newDosageInput.setAttribute("class", "adjust-field mt-8 sm:flex items-center w-72 text-left space-x-3 px-4 h-12 ring-1 ring-slate-900/10 rounded-lg text-slate-400");
+
+    adjust_field.replaceWith(newDosageInput);
+    
+    newDosageInput.addEventListener("input",function(){
+        data.weight = input.value;
+        data.prefer_dosage = newDosageInput.value;
+        calculateDosage(data);
+    });
 }
+
+function addDrugButton(drug){
+    var btn = document.createElement('button');
+    const newContent = document.createTextNode(drug);
+    btn.setAttribute("class", 'text-sm text-black  font-semibold h-6 px-4 m-1 rounded-lg items-center sm:w-auto ring-1 ring-gray-500	 ')
+    btn.setAttribute("id", drug);
+    btn.appendChild(newContent);
+    btn_holder.appendChild(btn);
+
+}
+
+function calculateDosage(data){
+    data.weight = input.value;
+    var dosage = data.weight*parseFloat(data.prefer_dosage)/parseFloat(data.density);
+    c_result.innerHTML = `<div class="calculate_result">dosage:${dosage.toFixed(2)} ml</div>`
+    console.log(`Dosage:${dosage},WEIGHT: ${data.weight} * prefer_dosage:${data.prefer_dosage} / Density:${data.density}`)
+}
+
 function showLoading(){
     output.innerHTML = `<div>Loading....</div>`
 }
 
 function showDrugData(data){
-    var dosage = data.results[0].properties.dosage_mgkg.rich_text[0].plain_text;
-    var prefer_dosage = data.results[0].properties.prefer_dosage.rich_text[0].plain_text;
-    var injection_way = [];
-    var injection_way_raw_array = data.results[0].properties.injection_way.multi_select;
-    injection_way_raw_array.forEach(element => injection_way.push(element.name));
-    var medicine_name = data.results[0].properties.medicine_name.title[0].plain_text;
-    var density = data.results[0].properties.density_mgml.number;
+    //TODO: 把資料在ＧＡＳ先處理好
+    output.innerHTML = `<a class="mb-2 text-xl font-bold underline">${data.medicine_name}</a><ul class="list-disc"><li>Dosage: ${data.dosage}</li><li>Prefer Dosage: ${data.prefer_dosage}</li><li>Injection way: ${data.injection_way}</li><li>Density: ${data.density}</li></ul>`;
 
-    output.innerHTML = `<ul><li>Dosage:${dosage}</li><li>Prefer Dosage:${prefer_dosage}</li><li>Injection way:${injection_way}</li><li>Medicine Name:${medicine_name}</li><li>Density:${density}</li></ul>`;
 }
-
-
-
-
 
 function fetchEndpointData(){
     console.log("clicked!!")
@@ -76,24 +112,4 @@ function fetchEndpointData(){
             console.log(data);
             printNeededData(data);
         })
-}
-
-function printNeededData(data){
-    var dosage = data.results[0].properties.dosage_mgkg.rich_text[0].plain_text;
-    console.log(`dosage:${dosage}`);
-    
-    var prefer_dosage = data.results[0].properties.prefer_dosage.rich_text[0].plain_text;
-    console.log(`prefer dosage:${prefer_dosage}`);
-    
-    var injection_way = [];
-    var injection_way_raw_array = data.results[0].properties.injection_way.multi_select;
-    injection_way_raw_array.forEach(element => injection_way.push(element.name));
-    console.log(injection_way);
-    
-    var medicine_name = data.results[0].properties.medicine_name.title[0].plain_text;
-    console.log(`medicine name:${medicine_name}`);
-    
-    var density = data.results[0].properties.density_mgml.number;
-    console.log(`density:${density}`);
-    //TODO: need solution for null value
 }
